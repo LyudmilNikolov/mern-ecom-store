fe0b67a365144d59c4e70db3468982be3afaf21c
Lyudmil Nikolov <nikolovlyudmill@gmail.com>
2024-03-19 19:54:06 +0200

    custom bootstrap template

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,16 @@
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import logo from '../assets/react.svg'
 
 function Header() {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Shop.com</Navbar.Brand>
+          <Navbar.Brand href='/'>
+            <img src={logo} alt='Shop.com'/>
+            Shop.com
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav'/>
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
